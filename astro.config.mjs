028ceac4c4d0393139954339efcd15582191194d
mrflippermen <99625153+mrflippermen@users.noreply.github.com>
2026-01-19 15:15:23 -0500

    qw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://flippermen.github.io',
     base: '/',
     output: 'static',
-    integrations: [sitemap()],
 });
